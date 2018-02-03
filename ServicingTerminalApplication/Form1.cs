4dd3cc2e23f0654b8048acc004b5ce6c55f980a8
Admin <Admin@LEOWHALES-2>
2018-02-03 18:53:16 +0800

    FIXED DIFFERENT SERVICING OFFICES

--- ServicingTerminalApplication/Form1.cs
+++ ServicingTerminalApplication/Form1.cs
@@ -13,7 +13,7 @@ namespace ServicingTerminalApplication
         Form2 form2 = new Form2();
         Form3 f3 = (Form3)Application.OpenForms["form3"];
         private String connection_string = System.Configuration.ConfigurationManager.ConnectionStrings["dbString"].ConnectionString;
-        static int Servicing_Office = 1;
+        static int Servicing_Office = 2;
         static int window = 1;
         static int modeCounter = 0;
         private static string id = string.Empty;
@@ -21,11 +21,13 @@ namespace ServicingTerminalApplication
         private static string full_name = string.Empty;
         private static string transaction_type = string.Empty;
         private static string type = string.Empty;
+        private static int transaction_type_id;
         private static int _pattern_max;
         private static int _pattern_current;
         //private static int
         DataTable table_Modes;
         DataTable table_Transactions;
+        DataTable table_Transactions_List;
         Form3 fnf = new Form3();
         public Form1()
         {
@@ -40,9 +42,83 @@ namespace ServicingTerminalApplication
             Console.Write("\n Initializing form... Calling getModes() to set table_Modes \n ");
             table_Modes = getModes();
             table_Transactions = getTransactionInfo();
+            table_Transactions_List = getTransactionList();
+            transaction_type_id = 0;
             _pattern_max = 0;
             _pattern_current = 0;
 
+        }
+        private bool checkIfNextCustomerExist(int q_cn)
+        {
+            SqlConnection con = new SqlConnection(connection_string);
+            using (con)
+            {
+                con.Open();
+                bool a = false;
+                String _query1 = "select TOP 1 Servicing_Office from Main_Queue where Queue_Number = @qn and Servicing_Office = @sn";
+                SqlCommand _cmd1 = new SqlCommand(_query1, con);
+                _cmd1.Parameters.AddWithValue("@sn", Servicing_Office);
+                _cmd1.Parameters.AddWithValue("@qn", q_cn);
+                object r = _cmd1.ExecuteScalar();
+                if (r != null) a = true;
+                else
+                {
+                    //code to handle the null case here...
+                    MessageBox.Show("False");
+                }
+                con.Close();
+                return a;
+
+            }
+        }
+        private DataTable getTransactionList()
+        {
+            DataTable transactionList = new DataTable();
+            transactionList.Columns.Add("Transaction_ID", typeof(int));
+            transactionList.Columns.Add("Servicing_Office", typeof(int));
+            transactionList.Columns.Add("Pattern_No", typeof(int));
+
+            SqlConnection con = new SqlConnection(connection_string);
+            using (con)
+            {
+                con.Open();
+                SqlCommand t_cmd = con.CreateCommand();
+                SqlDataReader t_rdr;
+
+                String t_q = "select * from Transaction_List";
+                t_cmd = new SqlCommand(t_q, con);
+
+                t_rdr = t_cmd.ExecuteReader();
+                while (t_rdr.Read())
+                {
+                    transactionList.Rows.Add(
+                       (int)t_rdr["Transaction_ID"],
+                       (int)t_rdr["Servicing_Office"],
+                       (int)t_rdr["Pattern_No"]);
+                    Console.Write(" getTransactions -> Added a row! ");
+                }
+                con.Close();
+            }
+            Console.Write(" \n returning transactionList... \n ");
+            return transactionList;
+        }
+        private int nextServicingOffice(int _pattern_no, int _transaction_id)
+        {
+            int a = 0;
+            int temp_pattern_no = 0;
+            int temp_transaction_id = 0;
+            foreach (DataRow row in table_Transactions_List.Rows)
+            {
+                temp_pattern_no = (int)row["Pattern_No"];
+                temp_transaction_id = (int)row["Transaction_ID"];
+                Console.Write(" \n retrieving the next servicing office ");
+                if (_transaction_id == temp_transaction_id && temp_pattern_no == _pattern_no)
+                {
+                    a = (int)row["Servicing_Office"];
+                    break;
+                }
+            }
+            return a;
         }
         private DataTable getTransactionInfo()
         {
@@ -150,7 +226,9 @@ namespace ServicingTerminalApplication
                 { rowCount++; { if (rowCount > 0) { break; } } }
                 if (rowCount > 0)
                 {
-                    //Retrieve queue info and set to variables
+                    // Check if there is people on queue
+
+                    // Retrieve queue info and set to variables
                     int q_cn = (int)rdr["Current_Number"];
                     int q_m = (int)rdr["Mode"];
                     string q_s = rdr["Status"].ToString();
@@ -193,9 +271,10 @@ namespace ServicingTerminalApplication
             String query = "select Current_Queue from Queue_Info where Servicing_Office = @Servicing_Office";
             cmd3 = new SqlCommand(query, con);
             cmd3.Parameters.AddWithValue("@Servicing_Office", q_so);
-            SqlDataReader rdr2;
-            rdr2 = cmd3.ExecuteReader();
-            while (rdr2.Read()) { res = (int)rdr2["Current_Queue"]; }
+            //SqlDataReader rdr2;
+            //rdr2 = cmd3.ExecuteReader();
+            //while (rdr2.Read()) { res = (int)rdr2["Current_Queue"]; }
+            res = (int)cmd3.ExecuteScalar();
             Console.Write("--RETURNING-> getQueueNumber[" + res + "]");
             
             return res;
@@ -203,74 +282,83 @@ namespace ServicingTerminalApplication
         private void setCustomerInformation(SqlConnection con, int q_cn) {
 
             // Called when next button is clicked.
-            // Calls consecutive functions for the next customer to be served.
+            if (checkIfNextCustomerExist(q_cn))
+            {
+                // Calls consecutive functions for the next customer to be served.
 
-            SqlCommand cmd4, _cmd4, _cmd0;
-            String query = "select TOP 1 Queue_Number,Type,Student_No,Full_name,Transaction_Type,Pattern_Current,Pattern_Max from Main_Queue where Queue_Number = @q_cn and Servicing_Office = @sn";
-            String _query_insert = "update Main_Queue set Pattern_Current = Pattern_Current + 1 where Queue_Number = @q_cn and Servicing_Office = @sn";
-            String _query_delete = "delete from Main_Queue where Queue_Number = @q_cn and Servicing_Office = @sn";
-            Console.Write(" \n \n deleting from main queue qn = " + q_cn + " ;; sn = " + Servicing_Office);
-            String _query_delete_queue_pattern = "delete from Queue_Transaction where Main_Queue_ID = @q_cn and Pattern_No = @q_pn";
-            cmd4 = new SqlCommand(query,con);
+                SqlCommand cmd4, _cmd4, _cmd0;
+                String query = "select TOP 1 Queue_Number,Type,Student_No,Full_name,Transaction_Type,Pattern_Current,Pattern_Max from Main_Queue where Queue_Number = @q_cn and Servicing_Office = @sn";
+                String _query_insert = "update Main_Queue set Servicing_Office = @q_nso, Pattern_Current = Pattern_Current + 1 where Queue_Number = @q_cn and Servicing_Office = @sn";
+                String _query_delete = "delete from Main_Queue where Queue_Number = @q_cn and Servicing_Office = @sn";
+                //Console.Write(" \n \n deleting from main queue qn = " + q_cn + " ;; sn = " + Servicing_Office);
+                String _query_delete_queue_pattern = "delete from Queue_Transaction where Main_Queue_ID = @q_cn and Pattern_No = @q_pn";
+                cmd4 = new SqlCommand(query, con);
 
-            cmd4.Parameters.AddWithValue("@q_cn", q_cn);
-            cmd4.Parameters.AddWithValue("@sn", Servicing_Office);
-            SqlDataReader rdr3;
-            rdr3 = cmd4.ExecuteReader();
-            while (rdr3.Read()) {
-                id = rdr3["Queue_Number"].ToString();
-                type = ((Boolean)rdr3["Type"] == false) ? "Student" : "Guest";
-                s_id = rdr3["Student_No"].ToString();
-                full_name = rdr3["Full_name"].ToString();
-                _pattern_max = (int)rdr3["Pattern_Max"];
-                _pattern_current = (int)rdr3["Pattern_Current"];
-                foreach (DataRow row in table_Transactions.Rows)
+                cmd4.Parameters.AddWithValue("@q_cn", q_cn);
+                cmd4.Parameters.AddWithValue("@sn", Servicing_Office);
+                SqlDataReader rdr3;
+                rdr3 = cmd4.ExecuteReader();
+                if (rdr3.Read())
                 {
-                    int _id = (int)row["Transaction_Type"];
-                    if (_id == (int)rdr3["Transaction_Type"])
+                    MessageBox.Show("HELLO! there is a Servicing_Office #2");
+                    id = rdr3["Queue_Number"].ToString();
+                    type = ((Boolean)rdr3["Type"] == false) ? "Student" : "Guest";
+                    s_id = rdr3["Student_No"].ToString();
+                    full_name = rdr3["Full_name"].ToString();
+                    _pattern_max = (int)rdr3["Pattern_Max"];
+                    _pattern_current = (int)rdr3["Pattern_Current"];
+                    transaction_type_id = (int)rdr3["Transaction_Type"];
+                    foreach (DataRow row in table_Transactions.Rows)
                     {
-                        transaction_type = (String)row["Transaction_Name"];
+                        int _id = (int)row["Transaction_Type"];
+                        if (_id == transaction_type_id)
+                        {
+                            transaction_type = (String)row["Transaction_Name"];
+                        }
                     }
+                    if (_pattern_current <= _pattern_max)
+                    {
+                        _cmd4 = new SqlCommand(_query_insert, con);
+                        _cmd4.Parameters.AddWithValue("@q_cn", q_cn);
+                        _cmd4.Parameters.AddWithValue("@sn", Servicing_Office);
+                        _cmd4.Parameters.AddWithValue("@q_nso", nextServicingOffice(_pattern_current, transaction_type_id));
+                        _cmd4.ExecuteNonQuery();
+                    }
+                    else
+                    {
+                        _cmd4 = new SqlCommand(_query_delete, con);
+                        _cmd4.Parameters.AddWithValue("@q_cn", q_cn);
+                        _cmd4.ExecuteNonQuery();
+                    }
+                    Console.Write("\n\n using _cmd0 -- q_cn = " + q_cn + " _pattern_current = " + _pattern_current);
+                    _cmd0 = new SqlCommand(_query_delete_queue_pattern, con);
+                    _cmd0.Parameters.AddWithValue("@q_cn", q_cn);
+                    _cmd0.Parameters.AddWithValue("@q_pn", _pattern_current);
+                    _cmd0.ExecuteNonQuery();
+
+                    updateForm nuea = new updateForm();
+                    nuea.id = id;
+                    nuea.type = type;
+                    nuea.s_id = s_id;
+                    nuea.full_name = full_name;
+                    nuea.transaction_type = transaction_type;
+                    fnf.OnFirstNameUpdated(nuea);
                 }
 
-                if (_pattern_current <= _pattern_max)
-                {
-                    _cmd4 = new SqlCommand(_query_insert, con);
-                    _cmd4.Parameters.AddWithValue("@q_cn", q_cn);
-                    _cmd4.Parameters.AddWithValue("@sn", Servicing_Office);
-                    _cmd4.ExecuteNonQuery();
-                }
-                else
-                {
-                    _cmd4 = new SqlCommand(_query_delete, con);
-                    _cmd4.Parameters.AddWithValue("@q_cn", q_cn);
-                    _cmd4.ExecuteNonQuery();
-                }
-                Console.Write("\n\n using _cmd0 -- q_cn = " + q_cn + " _pattern_current = " + _pattern_current);
-                _cmd0 = new SqlCommand(_query_delete_queue_pattern, con);
-                _cmd0.Parameters.AddWithValue("@q_cn", q_cn);
-                _cmd0.Parameters.AddWithValue("@q_pn", _pattern_current);
-                _cmd0.ExecuteNonQuery();
-            }
-            updateForm nuea = new updateForm();
-            nuea.id = id;
-            nuea.type = type;
-            nuea.s_id = s_id;
-            nuea.full_name = full_name;
-            nuea.transaction_type = transaction_type;
-            fnf.OnFirstNameUpdated(nuea);
-            //nuea.id = "wew";
-            //nuea.type = "wew";
-            //nuea.s_id = "wew";
-            //nuea.full_name = "wew";
-            //nuea.transaction_type = "wew";
-            //fnf.OnFirstNameUpdated(nuea);
-            Console.Write("updating using fnf...");
+                //nuea.id = "wew";
+                //nuea.type = "wew";
+                //nuea.s_id = "wew";
+                //nuea.full_name = "wew";
+                //nuea.transaction_type = "wew";
+                //fnf.OnFirstNameUpdated(nuea);
+                Console.Write("updating using fnf...");
+
+            }else { MessageBox.Show("Nibba doesn't exist."); }
         }
         private void Transfer_Customer_Logs() { }
         private void Transfer_Customer_Office() { }
         private void Mode_Check(SqlConnection con,int retrieved_counter) {
-            int x = 0, min = 0, max = 0, current_queue_number = 0, count_walkin = 0;
+            int min = 0, max = 0, current_queue_number = 0, count_walkin = 0;
             string mode_name = "xd";
             int mode_number = 0;
 
@@ -304,6 +392,8 @@ namespace ServicingTerminalApplication
                 // reset the counter, add a code (not here) to pick a mobile user
             }
             //return x;
+
+            
         }
         private int return_on_queue(SqlConnection con)
         {
